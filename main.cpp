d58dafd1c3e4b17bec524853ea4d855c8ff4ab1f
Rik Shaw <rikshaw76@gmail.com>
2016-05-05 18:12:10 +0300

    revert main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QLibraryInfo>
-#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-
-    QTranslator qtTranslator;
-    qtTranslator.load("qt_" + QLocale::system().name(),
-            QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-    app.installTranslator(&qtTranslator);
-
-    QTranslator myappTranslator;
-    myappTranslator.load("myapp_" + QLocale::system().name());
-    app.installTranslator(&myappTranslator);
-
-    MainWindow window(app.arguments());
-    window.show();
+    QApplication a(argc, argv);
+    MainWindow w(a.arguments());
+    w.show();
     
-    return app.exec();
+    return a.exec();
 }
